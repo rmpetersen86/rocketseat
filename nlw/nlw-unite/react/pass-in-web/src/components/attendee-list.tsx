03cf7ab830a64a8ec75119f3f3c4598e3a8a8633
rmpetersen86 <rafael.petersen86@gmail.com>
2024-04-03 22:41:30 -0300

    Aula 03 30:00

--- nlw/nlw-unite/react/pass-in-web/src/components/attendee-list.tsx
+++ nlw/nlw-unite/react/pass-in-web/src/components/attendee-list.tsx
@@ -7,16 +7,41 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { ChangeEvent, useState } from 'react'
-import { attendees } from '../data/attendees'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface Attendee {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  checkedInAt: string | null
+}
+
 export function AttendeeList() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
-  const totalPages = Math.ceil(attendees.length / 10)
+  const [page, setPage] = useState(1)  
+  const [total, setTotal] = useState(0)
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+
+  const totalPages = Math.ceil(total / 10)
+
+  useEffect(() => {
+    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+    url.searchParams.set('pageIndex', String(page - 1))
+    if (search.length > 0 ) {
+      url.searchParams.set('query', search)
+    }    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setAttendees(data.attendees)
+      setTotal(data.total)
+    })
+  },[page, search])
 
   function goToNextPage(){
     setPage(page + 1)
@@ -34,8 +59,10 @@ export function AttendeeList() {
     setPage(1)
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
     setSearch(event.target.value)
+    setPage(1)
   }
 
   return (
@@ -44,7 +71,12 @@ export function AttendeeList() {
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className='size-4 text-emerald-300'/>
-          <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:outline-none focus:ring-0" placeholder="Buscar participante..." />
+          <input 
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:outline-none focus:ring-0" 
+            placeholder="Buscar participante..." 
+            value={search}
+            onChange={onSearchInputChanged}
+            />
         </div>     
       </div>
       <Table>
@@ -61,7 +93,8 @@ export function AttendeeList() {
           </TableRow>
         </thead>
         <tbody>
-          {attendees.slice((page -1 ) * 10, page * 10).map((attendee) =>{
+        {/* {attendees.slice((page -1 ) * 10, page * 10).map((attendee) =>{ */}
+          {attendees.map((attendee) =>{
             return (
               <TableRow key={attendee.id}>
                 <TableCell><input type='checkbox' className='size-4 bg-black/20 rounded border border-white/10 checked:text-orange-400 checked:ring-0 focus:ring-0' /></TableCell>
@@ -73,7 +106,10 @@ export function AttendeeList() {
                   </div>
                 </TableCell>
                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                <TableCell>{dayjs().to(attendee.chekedInAt)}</TableCell>
+                <TableCell>
+                  {attendee.checkedInAt === null 
+                  ? <span className='text-zinc-500'>Não fez check-in</span>
+                  : dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>                  
                   <IconButton transparent>
                     <MoreHorizontal className='size-4'/>
@@ -86,7 +122,7 @@ export function AttendeeList() {
         <tfoot>
           <tr>
             <TableCell colSpan={3} className='py-3 px-4 text-sm text-zinc-300'>
-              Mostrando 10 de {attendees.length} itens
+              Mostrando {attendees.length} de {total} itens
             </TableCell>
             <TableCell colSpan={3} className='text-right'>
               <div className='inline-flex items-center gap-8'>
