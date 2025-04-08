b938a15a0161f76a9363cff04b2eaa0fbd1a837b
rmpetersen86 <rafael.petersen86@gmail.com>
2025-04-08 14:58:33 -0300

    aula reactNative

---
+++ reactNative/componentesAPI/ignitegym/App.tsx
@@ -0,0 +1,20 @@
+import { View, StatusBar } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { GluestackUIProvider, Text } from "@gluestack-ui/themed"
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+
+  return (
+    <GluestackUIProvider>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Text color='white' fontSize={34}>Home</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
+  );
+}
